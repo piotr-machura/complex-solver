d3c6a139624fecd6cc81798f523a27796084d82d
Piotr Machura <piotr.machura16@gmail.com>
2020-06-14 23:58:20 +0200

    updated jar to new spec

--- src/gui/FunctionFrame.java
+++ src/gui/FunctionFrame.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowFocusListener;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.awt.Toolkit;
